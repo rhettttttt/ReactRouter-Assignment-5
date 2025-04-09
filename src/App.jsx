1d3f7f0c0dd3a5e560645f717a44c648af612c15
rhett <rhett.yang@student.tdsb.on.ca>
2025-04-09 15:13:29 -0400

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeView from './views/HomeView';
+import LoginView from './views/LoginView';
+import RegisterView from './views/RegisterView';
+import MoviesView from './views/MoviesView';
+import ErrorView from './views/ErrorView';
+import MovieTileView from './views/MovieTileView';
+import MovieDetailView from './views/MovieDetailView';
+import ProtectedRoutes from './views/ProtectedRoutes';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/movies" element={<MoviesView />}>
+            <Route path="now_playing" element={<MovieTileView />}></Route>
+            <Route path="popular" element={<MovieTileView />}></Route>
+            <Route path="top_rated" element={<MovieTileView />}></Route>
+            <Route path="upcoming" element={<MovieTileView />}></Route>
+            <Route path=":id" element={<MovieDetailView />}></Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<ErrorView />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
