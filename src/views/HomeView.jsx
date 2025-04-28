facbc31c7a83d8c04d14f5ab730b7edeab023ded
Yang <Rhett.Yang@student.tdsb.on.ca>
2025-04-28 13:44:07 -0400

    stuff

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./HomeView.css";
+import Menu from "../../components/Menu";
+
+function HomeView() {
+  return (
+    <div>
+      <Menu />
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default HomeView;
